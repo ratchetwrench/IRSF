3ab3d79635d6793b5e573862acb4155d08565dac
ratchetwrench <david.r.wrench@gmail.com>
2017-09-28 12:18:10 -0700

    CDR WIP

--- cdr_generator.py
+++ cdr_generator.py
@@ -4,32 +4,27 @@ import numpy as np
 import random
 from numpy.random import normal
 import calendar
-
+from scipy.stats import expon
 
 # Load Data
-cdr = pd.read_csv('~/Downloads/cdr-2.csv', usecols=["from_number", "from_country"])
+# CDR_SAMPLES = pd.read_csv('src/data/cdr.csv', usecols=["from_country", "from_number"],
+#                           index_col="from_country")
 
-cdr_df = pd.read_csv('~/Downloads/cdr_data_prepared-2.csv', usecols=["country_name",
-                                                                     "country_proba",
-                                                                     "bbva_proba",
-                                                                     "operator_proba",
-                                                                     "operator_name",
-                                                                     "prefix"])
+CDR_SAMPLES = None
+CDR_PROBAS = pd.read_csv('src/data/cdr_data.csv', usecols=["country_name",
+                                                           "country_proba",
+                                                           "bbva_proba",
+                                                           "operator_proba",
+                                                           "operator_name",
+                                                           "prefix"])
 
+CDR_PROBAS.set_index(["country_name", "operator_name"], inplace=True)
+ADVANCED_COUNTRIES = CDR_PROBAS[CDR_PROBAS["bbva_proba"].isnull()]
+EMERGING_COUNTRIES = CDR_PROBAS.dropna()
 
-# Constants
-CDR_DATA = cdr_df.set_index("country_name")
-CDR_PROBAS = cdr_df.set_index(["country_name", "operator_name"]).fillna(0)
-CDR_SAMPLES = cdr.set_index("from_country").drop_duplicates(inplace=True)
-
-ADVANCED_COUNTRIES = cdr_df["country_name"].values
-EMERGING_COUNTRIES = cdr_df["country_name"][cdr_df["bbva_proba"] > 0].values
-OPERATORS = cdr_df[["country_name", "operator_name", "operator_proba"]].set_index(["country_name"])
-
-YEARS = np.array([2015, 2017])
-MONTHS = np.arange(1, 12 + 1)
-HOURS = np.arange(1, 24 + 1)
-MINUTES = np.arange(0, 59 + 1)
+# Set distributions to sample from
+CALL_CHARGE = expon(loc=365, scale=180)
+CALL_DURATION = expon(loc=365, scale=180)
 
 
 # Main Functionality
@@ -42,7 +37,7 @@ class CDR(object):
         self.national = count - self.international
         self.fraud = fraud
         self.count = count
-        self.records = {}
+        self.records = []
 
         if self.fraud:
             # Override defaults
@@ -50,7 +45,16 @@ class CDR(object):
             self.advanced_to_emerging = self.emerging_to_advanced  # swapped with self.advanced
             self.emerging_to_advanced = self.advanced_to_emerging  # swapped with self.emerging
             self.national = count - self.international
+            CALL_CHARGE = expon(loc=365, scale=180)
+            CALL_DURATION = expon(loc=365, scale=180)
 
+    @staticmethod
+    def writer(records):
+        with open('src/data/cdr.csv', 'a') as f:
+            for record in records:
+                f.write(record)
+
+    def bootstrap(self):
         # National Calls
         for record in range(self.national):
             self.cdr()
@@ -59,7 +63,7 @@ class CDR(object):
         for _ in range(self.international):
             self.cdr(international=True)
 
-        # Advaned Market Calls
+        # Advanced Market Calls
         for _ in range(self.emerging_to_advanced):
             self.cdr(advanced_to_emerging=True)
 
@@ -69,11 +73,12 @@ class CDR(object):
 
     @staticmethod
     def datetime_generator():
-        Y = np.random.choice([2015, 2017])
-        m = np.random.choice(MONTHS)
+        # Datetime ranges
+        Y = np.random.randint(2015, 2017)
+        m = np.random.randint(1, 12)
         d = np.random.choice(1, calendar.monthrange(Y, m)[1])
-        H = np.random.choice(HOURS)
-        M = np.random.choice(MINUTES)
+        H = np.random.randint(0, 60)
+        M = np.random.randint(0, 60)
         S = np.random.uniform(low=0.0, high=59.0)
         return "{}-{}-{} {:02d}:{:02d}:{:4f}".format(Y, m, d, H, M, S)
 
@@ -81,30 +86,30 @@ class CDR(object):
     def country_generator(emerging_to_advanced=False):
         # df.loc["<index 0 value>, <index 1 value>, ...][<column name>]
         if emerging_to_advanced:
-            return np.random.choice(EMERGING_COUNTRIES)
+            return np.random.choice(EMERGING_COUNTRIES.index.get_level_values('country_name'))
         else:
-            return np.random.choice(ADVANCED_COUNTRIES)
+            return np.random.choice(ADVANCED_COUNTRIES.index.get_level_values('country_name'))
 
     @staticmethod
     def operator_generator(country_name=None):
         # df.loc["<index 0 value>, <index 1 value>, ...][<column name>]
-        operators = OPERATORS.loc[country_name]["operator_name"]
-        p = OPERATORS.loc[country_name]["operator_proba"]
+        operators = CDR_PROBAS.loc[country_name]["operator_name"]
+        p = CDR_PROBAS.loc[country_name]["operator_proba"]
         return np.random.choice(operators, p=p)
 
     @staticmethod
     def phonenumber_generator(country_name=None):
 
         # get known phone numbers 80% of the time, otherwise generate a new one
-        if np.random.random() >= 0.8:
+        if CDR_SAMPLES and np.random.random() >= 0.8:
             return np.random.choice(CDR_SAMPLES.loc[country_name]["from_number"], size=1)
         else:
-            prefix = CDR_DATA.loc[country_name]["prefix"]
+            prefix = CDR_PROBAS.loc[country_name]["prefix"]
             if prefix == 1:
                 sn = random.randrange(2000000000, 9999999999)
                 return "+{}{}".format(prefix, sn)
             else:
-                sn = random.randrange(2000000, 999999999)
+                sn = random.randrange(2000000000, 999999999999)
                 return "+{}{}".format(prefix, sn)
 
     @staticmethod
@@ -113,8 +118,8 @@ class CDR(object):
 
     def cdr(self, international=False, advanced_to_emerging=False, emerging_to_advanced=False):
         record = {}
-        self.records["date_called"] = self.datetime_generator()
-        self.records["from_country"] = self.country_generator()
+        record["date_called"] = self.datetime_generator()
+        record["from_country"] = self.country_generator()
 
         if international:
             record["to_country"] = self.country_generator()
@@ -123,8 +128,8 @@ class CDR(object):
         elif emerging_to_advanced:
             record["to_country"] = self.country_generator(emerging_to_advanced=True)
         else:
-            record["from_number"] = self.phonenumber_generator(country_name=['from_country'])
-            record["from_operator"] = self.operator_generator(country_name=record['from_country'])
+            record["from_number"] = self.phonenumber_generator(country_name=record['from_country'])
+            record["from_operator"] = self.operator_generator(country_name=record['from_operator'])
 
         if np.random.random() >= 0.8:
             record["extension"] = self.extension_generator()
@@ -133,18 +138,13 @@ class CDR(object):
 
         record["to_country"] = record['from_country']
         record["to_number"] = self.phonenumber_generator(country_name=record['to_country'])
-        record["to_operator"] = self.operator_generator(country_name=record['to_country'])
+        record["to_operator"] = self.operator_generator(country_name=record['to_operator'])
+        record["call_duration"] = CALL_DURATION.rvs(size=1)
+        record["call_charge"] = CALL_CHARGE.rvs(size=1)
 
-        if self.fraud:
-            record["call_duration"] = np.random.exponential(scale=.4)
-            record["call_charge"] = np.random.exponential(scale=.015)
-        else:
-            record["call_duration"] = np.random.exponential(scale=.4)
-            record["call_charge"] = np.random.exponential(scale=.015)
-
-        return self.records.update(record)
+        return self.records.append(record)
 
 
 if __name__ == '__main__':
     x = CDR(count=100, fraud=False)
-    print(x)
+    x.bootstrap()
